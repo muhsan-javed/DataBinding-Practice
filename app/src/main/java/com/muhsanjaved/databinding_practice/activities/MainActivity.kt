6c5bd7fec2b48b390fbc90e2dd1ff6ccbd414a2c
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-04-04 08:16:03 +0500

    how to used binding Adapter

--- app/src/main/java/com/muhsanjaved/databinding_practice/activities/MainActivity.kt
+++ app/src/main/java/com/muhsanjaved/databinding_practice/activities/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.muhsanjaved.databinding_practice.R
+import com.muhsanjaved.databinding_practice.adapters.ProductsAdapter
 import com.muhsanjaved.databinding_practice.databinding.ActivityMainBinding
 import com.muhsanjaved.databinding_practice.fragments.MainFragment
+import com.muhsanjaved.databinding_practice.models.DataProvider
 import com.muhsanjaved.databinding_practice.viewModels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -31,11 +34,15 @@ class MainActivity : AppCompatActivity() {
 //        binding.tvFirst.text = viewModel.user.name
 //        binding.tvFirst.append("Muhsan")
 
+        //setContentView(R.layout.activity_main)
 
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        supportFragmentManager.beginTransaction()
+//            .add(R.id.main_fragment_container, MainFragment(), "main_fragment")
+//            .commit()
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.main_fragment_container, MainFragment(), "main_fragment")
-            .commit()
+        binding.rvProducts.layoutManager = GridLayoutManager(this, 2)
+        binding.rvProducts.adapter = ProductsAdapter(this,DataProvider.productList.toList())
+
     }
 }
---
+++ app/src/main/java/com/muhsanjaved/databinding_practice/adapters/ProductsAdapter.kt
@@ -0,0 +1,65 @@
+package com.muhsanjaved.databinding_practice.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.muhsanjaved.databinding_practice.databinding.ProductItemBinding
+import com.muhsanjaved.databinding_practice.models.Product
+
+class ProductsAdapter(private val context: Context, private val dataList: List<Product>) :
+    RecyclerView.Adapter<ProductsAdapter.BindingViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder {
+//        val rootView = LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
+        val rootView = ProductItemBinding.inflate(LayoutInflater.from(context), parent, false)
+        return BindingViewHolder(rootView)
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: BindingViewHolder, position: Int) {
+        val product = dataList[position]
+
+        holder.itemBinding.productItem = product
+        holder.itemBinding.executePendingBindings()
+
+      /*  holder.tvName.text = product.name
+        holder.tvPrice.text = "${product.price}$ "
+        holder.tvRating.rating = product.rating
+        holder.tvNumRating.text = "(${product.totalRatings})"
+
+        try {
+            val bitmap = BitmapFactory.decodeStream(context.assets.open(product.image))
+            holder.ivProduct.setImageBitmap(bitmap)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        holder.ivSale.visibility = if (product.salePrice > 0) View.VISIBLE else View.GONE*/
+    }
+
+    class BindingViewHolder(val itemBinding:ProductItemBinding) : RecyclerView.ViewHolder(itemBinding.root)
+
+
+
+  /*  class MyViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
+
+        val tvName = itemView.findViewById<TextView>(R.id.tv_name)
+        val tvPrice = itemView.findViewById<TextView(R.id.tv_price)
+        val tvRating = itemView.findViewById<RatingBar(R.id.rating_bar)
+        val tvNumRating = itemView.findViewById<TextView>(R.id.tv_num_rating)
+
+        val ivProduct = itemView.findViewById<ImageView>(R.id.iv_product)
+        val ivSale = itemView.findViewById<ImageView>(R.id.iv_product_sale)
+
+       */
+    /* val tvName = itemView.tv_name
+        val tvPrice = itemView.tv_price
+        val tvRating = itemView.rating_bar
+        val tvNumRating = itemView.tv_num_rating
+        val ivProduct = itemView.iv_product
+        val ivSale = itemView.iv_product_sale
+*//*
+    }*/
+}
